bcb67932f0c167b9291c68417fcc8f6e6a2e4c4c
Seung Park <seungpark222@gmail.com>
2015-11-18 18:16:41 -0500

    edit validation

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,6 +1,6 @@
 class Tagging < ActiveRecord::Base
   validates :photo_id, :tag_id, presence: true, numericality: { only_integer: true }
-  validate :user_is_owner, :before => :create
+  # validate :user_is_owner, :before => :create
 
   belongs_to :tag
   belongs_to :photo
