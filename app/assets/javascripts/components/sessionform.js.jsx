c7dad2ef5d43b10813ca36be2b7185b8b07f9dbc
Seung Park <seungpark222@gmail.com>
2015-11-26 14:54:26 -0500

    add guest sign in

--- app/assets/javascripts/components/sessionform.js.jsx
+++ app/assets/javascripts/components/sessionform.js.jsx
@@ -5,32 +5,44 @@
     mixins: [ReactRouter.History],
 
     submit: function(e) {
-      e.preventDefault();
+      debugger
+      if (typeof e !== "undefined") {
+        e.preventDefault();
+      }
 
-      var credentials = $(e.currentTarget).serializeJSON();
+      var credentials = $(React.findDOMNode(this.refs.form)).serializeJSON();
       SessionsApiUtil.signin(credentials, function () {
         this.history.pushState(null, "/");
       }.bind(this));
     },
 
+    _fillGuestInfo: function(e){
+      e.preventDefault();
+      React.findDOMNode(this.refs.username).value = "guest";
+      React.findDOMNode(this.refs.password).value = "hello123";
+      this.submit();
+      debugger
+    },
+
     render: function() {
       return (
-        <div className="signin-form">
+        <div className="signin-page">
           <Header/>
-          <form onSubmit={this.submit}>
+          <form className="signin-form" onSubmit={this.submit} ref="form">
           <h1>Sign In!</h1>
 
-          <label>
+          <label className="signin-username">
             Username
-            <input type="text" name="username" />
+            <input type="text" name="username" ref="username"/>
           </label>
 
-          <label>
+          <label className="signin-password">
             Password
-            <input type="password" name="password" />
+            <input type="password" name="password" ref="password"/>
           </label>
 
-          <button>Sign In!</button>
+          <button className="signin-button">Sign In!</button>
+          <button className="signin-guest" onClick={this._fillGuestInfo}>Guest Account!</button>
 
         </form>
         </div>
