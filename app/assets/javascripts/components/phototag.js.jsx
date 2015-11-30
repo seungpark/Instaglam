918cb6a2d3e943e4f34429095a10a2c5b1655805
Seung Park <seungpark222@gmail.com>
2015-11-30 15:53:01 -0500

    fix link to tag page

--- app/assets/javascripts/components/phototag.js.jsx
+++ app/assets/javascripts/components/phototag.js.jsx
@@ -3,12 +3,13 @@
   root.PhotoTags = React.createClass({
 
     render: function() {
+      debugger
       return (
         <div className="index-photo-tag-container">
           <h3 className="index-photo-tag-header">Tags</h3>
           <div className="index-photo-tags">
               {this.props.tags.map(function(tag){
-                return <a href="/#">{tag.name}</a>;
+                return <a href={"/#/tags/" + tag.id}>{tag.name}</a>;
               })}
           </div>
         </div>
