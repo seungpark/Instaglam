326d1e15049e1d72c2d0184925d7935428cf8b09
Seung Park <seungpark222@gmail.com>
2015-12-05 00:38:39 -0500

    update seed profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,18 @@
 
 users = User.create([
   {username: "seung", password: "hello123", bio: "travel the world", name: "Seung Park"},
-  {username: "guest", password: "hello123", bio: "this is my bio", name: "John Doe"},
-  {username: "guest2", password: "hello123", bio: "i am a sample profile", name: "Adam Smith"},
-  {username: "guest3", password: "hello123", bio: "sample test bio", name: "Jane"},
-  {username: "guest4", password: "hello123", bio: "what is life", name: "Peter"}
+  {username: "anthonydavis", password: "hello123", bio: "how you like my brow", name: "Anthony Davis"},
+  {username: "jamesharden", password: "hello123", bio: "FEAR the BEARD", name: "James Harden"},
+  {username: "chandlerbing", password: "hello123", bio: "Can I BE any funnier?", name: "Chandler Bing"},
+  {username: "jimmyfallon", password: "hello123", bio: "LADIES AND GENLEMEN", name: "Jimmy Fallon"},
+  {username: "johncena", password: "hello123", bio: "JOHN CENA", name: "JOHN CENA"},
+  {username: "lebronjames", password: "hello123", bio: "the king", name: "LeBron James"},
+  {username: "carmelo", password: "hello123", bio: "", name: "Carmelo Anthony"},
+  {username: "mikekrieger", password: "hello123", bio: "welcome to instaglam", name: "Mike Krieger"},
+  {username: "obama", password: "hello123", bio: "For the People", name: "Barack Obama"},
+  {username: "smiley", password: "hello123", bio: "I'm happy", name: "Smiley Face"},
+  {username: "curry", password: "hello123", bio: "Chef Curry", name: "Steph Curry"},
+
 ])
 
 
