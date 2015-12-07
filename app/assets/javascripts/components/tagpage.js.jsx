bfc5b8f2124d67f322c0bb9a325fca66c5279fd7
Seung Park <seungpark222@gmail.com>
2015-12-07 14:58:07 -0500

    add styling to tag page buttons

--- app/assets/javascripts/components/tagpage.js.jsx
+++ app/assets/javascripts/components/tagpage.js.jsx
@@ -6,7 +6,8 @@
       return({
         photos: PhotoStore.all(),
         tagid: parseInt(this.props.location.pathname.slice(6)),
-        tagname: ""
+        tagname: "",
+        view: "grid"
       });
     },
 
@@ -19,6 +20,23 @@
           }
         }.bind(this)).name
       });
+    },
+
+
+    _changeToGrid: function() {
+      if (this.state.view === "list") {
+        this.setState({ view: "grid" });
+        document.getElementsByClassName("active-icon")[0].setAttribute("class", "list-icon");
+        document.getElementsByClassName("grid-icon")[0].setAttribute("class", "active-icon");
+      }
+    },
+
+    _changeToList: function() {
+      if (this.state.view === "grid") {
+        this.setState({ view: "list" });
+        document.getElementsByClassName("active-icon")[0].setAttribute("class", "grid-icon");
+        document.getElementsByClassName("list-icon")[0].setAttribute("class", "active-icon");
+      }
     },
 
     componentWillMount: function() {
@@ -38,31 +56,64 @@
     },
 
     render: function() {
-      return(
-        <div className="tagpage">
-          <ul className="tagpage-ul">
-            <li className="tagpage-header">
-              <h1 className="tagpage-header">{"#" + this.state.tagname}</h1>
-            </li>
-            {this.state.photos.map(function (photo) {
-              return <IndexPhoto
-                key={photo.id}
-                photo={photo}
-                author={photo.user}
-                comments={photo.comments}
-                likes={photo.likes}
-                user={CurrentUserStore.currentUser()}
-                source={"tagpage"}
-                followedUserIds={this.props.followedUserIds}
-                tags={photo.tags}
-                tagid={parseInt(this.props.location.pathname.slice(6))}
-                />;
-            }.bind(this) )}
+      if (this.state.view === "grid") {
+        return(
+          <div className="tagpage">
+            <div className="tagpage-grid">
+              <div className="tagpage-header">
+                <h1 className="tagpage-header">{"#" + this.state.tagname}</h1>
+                <div className="viewing-options">
+                  <input type="image" className="active-icon" src={assets.gridview_icon} onClick={this._changeToGrid}></input>
+                  <input type="image" className="list-icon" src={assets.listview_icon} onClick={this._changeToList}></input>
+                </div>
+              </div>
+              <ul className="tagpage-ul">
+                {this.state.photos.map(function (photo) {
+                  return (
+                    <div className="square-photo-container">
+                      <a href={"/#/photos/" + photo.id}>
+                      <img className="square-photo" src={photo.image_url} />
+                      </a>
+                    </div>
+                  );
+                }.bind(this) )}
 
-          </ul>
-        </div>
-      );
+              </ul>
+            </div>
+          </div>
+        );
+      }else if (this.state.view === "list") {
+        return(
+          <div className="tagpage">
+            <div className="tagpage-list">
+                <div className="tagpage-header">
+                  <h1 className="tagpage-header">{"#" + this.state.tagname}</h1>
+                  <div className="viewing-options">
+                    <input type="image" className="active-icon" src={assets.gridview_icon} onClick={this._changeToGrid}></input>
+                    <input type="image" className="list-icon" src={assets.listview_icon} onClick={this._changeToList}></input>
+                  </div>
+                </div>
+              <ul className="tagpage-ul">
+                {this.state.photos.map(function (photo) {
+                  return <IndexPhoto
+                    key={photo.id}
+                    photo={photo}
+                    author={photo.user}
+                    comments={photo.comments}
+                    likes={photo.likes}
+                    user={CurrentUserStore.currentUser()}
+                    source={"tagpage"}
+                    followedUserIds={this.props.followedUserIds}
+                    tags={photo.tags}
+                    tagid={parseInt(this.props.location.pathname.slice(6))}
+                    />;
+                }.bind(this) )}
 
+              </ul>
+            </div>
+          </div>
+        );
+      }
     }
 
   });
