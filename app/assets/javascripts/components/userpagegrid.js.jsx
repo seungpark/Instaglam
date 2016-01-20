0692f36e95cc47e1711035b0b741910752b516fb
Seung Park <seungpark222@gmail.com>
2016-01-20 18:18:46 -0500

    hovering over photo on square view shows comment and like count

--- app/assets/javascripts/components/userpagegrid.js.jsx
+++ app/assets/javascripts/components/userpagegrid.js.jsx
@@ -5,7 +5,18 @@
     mixins: [ReactRouter.History],
 
     _imageload: function(e) {
-      $(".loading#" + e.currentTarget.id).addClass("hide");
+      $('#' + e.currentTarget.id + '.loading').addClass("hide");
+    },
+
+    _hoverImage: function(e) {
+      $('#' + e.currentTarget.id + '.hovering').removeClass('hide');
+      $('#' + e.currentTarget.id + '.stats').removeClass('hide');
+      debugger
+    },
+
+    _unhoverImage: function(e) {
+      $('#' + e.currentTarget.id + '.hovering').addClass('hide');
+      $('#' + e.currentTarget.id + '.stats').addClass('hide');
     },
 
     render: function() {
@@ -15,10 +26,15 @@
           {this.props.photos.map(function (photo) {
 
             return (
-              <div className="square-photo-container">
+              <div className="square-photo-container" id={photo.id} onMouseOver={this._hoverImage} onMouseOut={this._unhoverImage}>
                 <a href={"/#/photos/" + photo.id}>
                 <div className="loading" id={photo.id} />
-                <img className="square-photo" src={photo.image_url} id={photo.id} onLoad={this._imageload}/>
+                <div className="hovering hide" id={photo.id}/>
+                <div className="stats hide" id={photo.id}>
+                  <img src={assets.transparentHeart}/> {photo.likes.length}
+                  <img src={assets.comment} className="comment-icon"/> {photo.comments.length}
+                </div>
+                <img className="square-photo" src={photo.image_url} id={photo.id} onLoad={this._imageload} />
                 </a>
               </div>
             );
