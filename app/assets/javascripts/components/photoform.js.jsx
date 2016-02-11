19505d820f142a5dfed4345f53e5f9877582b296
Seung Park <seungpark222@gmail.com>
2016-02-11 16:06:08 -0500

    fix typo of state.file to state.imageFile

--- app/assets/javascripts/components/photoform.js.jsx
+++ app/assets/javascripts/components/photoform.js.jsx
@@ -70,7 +70,8 @@
 
     handleSubmit: function(e) {
       e.preventDefault();
-      if (this.state.title.length === 0 || typeof this.state.file === "undefined") {
+      debugger
+      if (this.state.title.length === 0 || typeof this.state.imageFile === "undefined") {
         if (typeof $("#error")[0] === "undefined") {
           $(".add-photo-form").append("<div id='error'>Title and Image are required</div>");
         }
